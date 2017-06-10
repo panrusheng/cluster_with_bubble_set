7853c2685241c91552b3dc3955fa3eb3f7c661d9
panrusheng <panrusheng@zju.edu.cn>
2017-06-10 13:28:42 +0800

    'initialization'

---
+++ project/test_data.js
@@ -0,0 +1,17 @@
+/**
+ * Created by prs on 2017/6/10.
+ */
+const SIZE = 8;
+var nodes = ["A","B","C","D","E","F","G","H"];
+var links = [
+    {"source": "A", "target": "B"},
+    {"source": "C", "target": "B"},
+    {"source": "D", "target": "B"},
+    {"source": "E", "target": "B"},
+    {"source": "D", "target": "G"},
+    {"source": "E", "target": "G"},
+    {"source": "F", "target": "G"},
+    {"source": "H", "target": "G"},
+    {"source": "D", "target": "E"},
+    {"source": "D", "target": "F"},
+    {"source": "E", "target": "H"},];
