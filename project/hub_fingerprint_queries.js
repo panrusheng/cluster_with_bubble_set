a6ea800f2aa9db76ed7b1c896f062af396c12dd0
panrusheng <panrusheng@zju.edu.cn>
2017-06-14 11:41:08 +0800

    add hub_fingerprint_query method

--- project/hub_fingerprint_queries.js
+++ project/hub_fingerprint_queries.js
@@ -2,10 +2,7 @@
  * Created by prs on 2017/6/13.
  */
 function f1(hub,node){//fingerprint with hub
-    if(graph[hub][node]===1||graph[node][hub]===1){
-        return 1;
-    }
-    return 0;
+    return graph[hub][node];
 }
 // how to choose the hub and query???
 // ...
